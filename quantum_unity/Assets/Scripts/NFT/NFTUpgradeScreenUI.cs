8d1e8cd3e7654ec2cdcbf6ac83dddba950fce36e
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2024-04-02 23:13:49 -0600

    Github push bug test

--- quantum_unity/Assets/Scripts/NFT/NFTUpgradeScreenUI.cs
+++ quantum_unity/Assets/Scripts/NFT/NFTUpgradeScreenUI.cs
@@ -6,7 +6,6 @@ public class NFTUpgradeScreenUI : MonoBehaviour
     public TMP_Text levelInfoText;
     public TMP_Text hpInfoText;
     public TMP_Text damageInfoText;
-    public Animator upgradeAnimator; // Reference to an Animator component for animations
 
     // Method to activate the upgrade screen and display information
     public void ActivateUpgradeScreen(int currentLevel, int updatedLevel, int currentHP, int hpDiff, int currentDamage, int damageDiff)
@@ -18,7 +17,5 @@ public class NFTUpgradeScreenUI : MonoBehaviour
         hpInfoText.text = $"HP: {currentHP} + {hpDiff}";
         damageInfoText.text = $"Damage: {currentDamage} + {damageDiff}";
 
-        // Trigger animation
-        upgradeAnimator.SetTrigger("UpgradeSuccess"); // Assuming there's a trigger set up in your Animator
     }
 }
