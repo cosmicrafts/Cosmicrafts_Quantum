c48f6b312dbaafef74f5b51f57b34d16d1575d76
ohsalmeron <ohsalmeron@gmail.com>
2024-04-05 03:53:07 -0600

    error fix = sprites null

--- quantum_unity/Assets/Scripts/ELO/LeagueManager.cs
+++ quantum_unity/Assets/Scripts/ELO/LeagueManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
+using TMPro;
 
 public class LeagueManager : MonoBehaviour
 {
     public static LeagueManager Instance { get; private set; } // Singleton instance
     public List<League> leagues;
     public Image leagueSpriteImage;
+    public TMP_Text leagueNameText;
 
     private void Awake()
     {
@@ -36,7 +39,7 @@ public class LeagueManager : MonoBehaviour
     void OnDestroy()
     {
         // Always good practice to unsubscribe from events to avoid memory leaks.
-        EloManagement.Instance.OnLeagueChanged -= UpdateLeagueSpriteUI;
+        EloManagement.Instance.OnLeagueChanged -= UpdateLeagueUI;
     }
 
     private void HandleLeagueChange(League newLeague)
@@ -49,46 +52,44 @@ public class LeagueManager : MonoBehaviour
         leagues = new List<League>
         {
             // Begin at the middle and expand both ways
-            new League { name = "Nebula League: Stardust Pioneers", minElo = int.MinValue, maxElo = 1199, leagueSprite = /* Assign sprite */ },
-            new League { name = "Nebula League: Cosmic Sparks", minElo = 1200, maxElo = 1299, leagueSprite = /* Assign sprite */ },
-            new League { name = "Nebula League: Nova Travelers", minElo = 1300, maxElo = 1399, leagueSprite = /* Assign sprite */ },
+            new League { name = "Nebula League: Stardust Pioneers", minElo = int.MinValue, maxElo = 1199, leagueSprite = null},
+            new League { name = "Nebula League: Cosmic Sparks", minElo = 1200, maxElo = 1299, leagueSprite = null},
+            new League { name = "Nebula League: Nova Travelers", minElo = 1300, maxElo = 1399, leagueSprite = null},
 
-            new League { name = "Asteroid League: Comet Chasers", minElo = 1400, maxElo = 1499, leagueSprite = /* Assign sprite */ },
-            new League { name = "Asteroid League: Asteroid Miners", minElo = 1500, maxElo = 1599, leagueSprite = /* Assign sprite */ },
-            new League { name = "Asteroid League: Meteor Ascendants", minElo = 1600, maxElo = 1699, leagueSprite = /* Assign sprite */ },
+            new League { name = "Asteroid League: Comet Chasers", minElo = 1400, maxElo = 1499, leagueSprite = null},
+            new League { name = "Asteroid League: Asteroid Miners", minElo = 1500, maxElo = 1599, leagueSprite = null},
+            new League { name = "Asteroid League: Meteor Ascendants", minElo = 1600, maxElo = 1699, leagueSprite = null},
 
-            new League { name = "Moon League: Volcanic Moons", minElo = 1700, maxElo = 1799, leagueSprite = /* Assign sprite */ },
-            new League { name = "Moon League: Ice Moons", minElo = 1800, maxElo = 1899, leagueSprite = /* Assign sprite */ },
-            new League { name = "Moon League: Terra Moons", minElo = 1900, maxElo = 1999, leagueSprite = /* Assign sprite */ },
+            new League { name = "Moon League: Volcanic Moons", minElo = 1700, maxElo = 1799, leagueSprite = null},
+            new League { name = "Moon League: Ice Moons", minElo = 1800, maxElo = 1899, leagueSprite = null},
+            new League { name = "Moon League: Terra Moons", minElo = 1900, maxElo = 1999, leagueSprite = null},
 
-            new League { name = "Planetary League: Dwarf Planets", minElo = 2000, maxElo = 2099, leagueSprite = /* Assign sprite */ },
-            new League { name = "Planetary League: Rocky Planets", minElo = 2100, maxElo = 2199, leagueSprite = /* Assign sprite */ },
-            new League { name = "Planetary League: Gas Giants", minElo = 2200, maxElo = 2299, leagueSprite = /* Assign sprite */ },
+            new League { name = "Planetary League: Dwarf Planets", minElo = 2000, maxElo = 2099, leagueSprite = null},
+            new League { name = "Planetary League: Rocky Planets", minElo = 2100, maxElo = 2199, leagueSprite = null},
+            new League { name = "Planetary League: Gas Giants", minElo = 2200, maxElo = 2299, leagueSprite = null},
 
-            new League { name = "Star League: Red Dwarfs", minElo = 2300, maxElo = 2399, leagueSprite = /* Assign sprite */ },
-            new League { name = "Star League: Blue Giants", minElo = 2400, maxElo = 2499, leagueSprite = /* Assign sprite */ },
-            new League { name = "Star League: Supernova", minElo = 2500, maxElo = 2599, leagueSprite = /* Assign sprite */ },
+            new League { name = "Star League: Red Dwarfs", minElo = 2300, maxElo = 2399, leagueSprite = null},
+            new League { name = "Star League: Blue Giants", minElo = 2400, maxElo = 2499, leagueSprite = null},
+            new League { name = "Star League: Supernova", minElo = 2500, maxElo = 2599, leagueSprite = null},
 
-            new League { name = "Galactic League: Dwarf Galaxies", minElo = 2600, maxElo = 2699, leagueSprite = /* Assign sprite */ },
-            new League { name = "Galactic League: Spiral Galaxies", minElo = 2700, maxElo = 2799, leagueSprite = /* Assign sprite */ },
-            new League { name = "Galactic League: Quasar", minElo = 2800, maxElo = 2899, leagueSprite = /* Assign sprite */ },
+            new League { name = "Galactic League: Dwarf Galaxies", minElo = 2600, maxElo = 2699, leagueSprite = null},
+            new League { name = "Galactic League: Spiral Galaxies", minElo = 2700, maxElo = 2799, leagueSprite = null},
+            new League { name = "Galactic League: Quasar", minElo = 2800, maxElo = 2899, leagueSprite = null},
 
-            new League { name = "Event Horizon League: Singularity Titans", minElo = 2900, maxElo = int.MaxValue, leagueSprite = /* Assign sprite */ },
+            new League { name = "Event Horizon League: Singularity Titans", minElo = 2900, maxElo = int.MaxValue, leagueSprite = null},
         };
     }
 
-    // Method to get current league based on Elo
     public League GetCurrentLeague(double elo)
     {
         return leagues.FirstOrDefault(l => elo >= l.minElo && elo <= l.maxElo);
     }
 
-    public void UpdateLeagueSpriteUI(League newLeague)
+    private void UpdateLeagueUI(League newLeague)
     {
-        // Check if the newLeague object is valid and has an associated sprite.
-        if (newLeague != null && newLeague.leagueSprite != null)
+        if (newLeague != null)
         {
-            // Update the UI Image component with the new league's sprite.
+            leagueNameText.text = newLeague.name;
             leagueSpriteImage.sprite = newLeague.leagueSprite;
         }
     }
